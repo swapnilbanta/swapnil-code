d180a6a315093b3c80200f4d109b5434744b1c40
swapnilbanta <swapnilbanta@swapnilbantas-MacBook-Air.local>
2025-10-01 13:24:52 +0530

    code changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://142.93.215.17", // 👈 your backend
+        changeOrigin: true,
+        secure: false, // allow http
+      },
+    },
+  },
 })
